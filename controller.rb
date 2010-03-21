3fd8359e8447332f27e521f9be871e2f919b3788
jugyo <jugyo.org@gmail.com>
2010-03-22 00:10:59 +0900

    changed to show output and error

--- controller.rb
+++ controller.rb
@@ -1,9 +1,23 @@
+require 'stringio'
+
 class Controller
   attr_accessor :inTextField, :outTextField
 
   def evalText(sender)
+    stdout = StringIO.new
+
     text = @inTextField.textStorage.mutableString
-    outText = eval(text).inspect
-    @outTextField.appendText(outText)
+
+    begin
+      $stdout = stdout
+      outText = eval(text).inspect
+    rescue Exception => e
+      @outTextField.appendText(e.message + "\n")
+    ensure
+      $stdout = STDOUT
+    end
+
+    @outTextField.appendText(stdout.string)
+    @outTextField.appendText("=> " + outText + "\n")
   end
 end
